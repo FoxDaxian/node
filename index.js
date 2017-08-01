616eda599c0a3b967baad6364af57ceb920b8c6a
a13821190779 <945039036@qq.com>
2017-08-02 00:08:28 +0800

    Update: [service] 更改数据库地址，配置express-session

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser') // 需要这个中间件来处理post请求
 const cookieParser = require('cookie-parser') // 解析cookies的
-// const session = require('express-session') // 创建session的
+const session = require('express-session') // 创建session的
+const MongoStore = require('connect-mongo')(session) // session存储的地方，express-session默认存储在内存里，但是易丢失
 const config = require('./config/')
 const commonRouter = require('./routers/common.router.js')
 
@@ -12,7 +13,7 @@ global.Promise = bluebird
 //连接数据库
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise // mongoose的Promise被弃用了...
-mongoose.connect('mongodb://localhost/blog')
+mongoose.connect('mongodb://127.0.0.1:27017/blog')
 const db = mongoose.connection
 db.on('error', (error) => {
 	console.log(`连接数据库失败：${error}`)
@@ -27,29 +28,28 @@ app.use('/static', express.static('public'))//设置资源目录
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-// 解释cookise的
+// 解析cookise的
 app.use(cookieParser())
-// app.use(session({
-// 	resave: false,
-// 	saveUninitialized: true,
-// 	secret: RandomSecret(),
-// 	name: 'blogSession',
-// 	cookie: { maxAge: 60 * 1000}
-// }))
+// 配置session
+app.use(session({
+	// secure字段: 应用在https
+	name: 'blogSession',
+	resave: false,
+	// 是指无论有没有session cookie，每次请求都设置个session cookie
+	saveUninitialized: false,
+	// 加密
+	secret: '40f908a38ecdbf40984e671970bbb0a2fdbbaef48919afefa42bfa21a13f31',
+	// 过期时间
+	cookie: { maxAge	: 7 * 24 * 60 * 60 * 1000},
+	// session的存储方式，默认存在内存中，但是会丢失，存在数据库中则不会
+	store: new MongoStore({
+		url: 'mongodb://127.0.0.1:27017/blog'
+	})
+}))
+
 
 // use 路由
-app.use( commonRouter )
-
-// 测试 cookieParser 
-app.get('/', function (req, res) {
-    if (req.cookies.visit) {
-        res.cookie('visit', +req.cookies.visit + 1, {maxAge: 10000})
-        res.send("再次欢迎访问")
-    } else {
-        res.cookie('visit', 1, {maxAge: 10000})
-        res.send("欢迎首次访问")
-    }
-})
+app.use( '/api', commonRouter )
 
 // 配置文件选项，监听
 app.listen(config.port)
