6f5aa3387b88b155a9940517dab3ff5bfaeddab2
a13821190779 <945039036@qq.com>
2017-08-02 00:09:13 +0800

    Update: [service] 增加跨域携带cookies的header

--- routers/common.router.js
+++ routers/common.router.js
@@ -5,7 +5,9 @@ const router = express.Router({
 const userRouter = require('./user.router.js')
 
 router.use(( req, res, next ) => {
-	res.header('Access-Control-Allow-Origin', '*')
+	// 跨域接受cookies，进行身份验证
+	res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
+	res.header('Access-Control-Allow-Credentials', true)
 	res.header('Access-Control-Allow-Headers', 'X-Requested-With')
 	res.header('Access-Control-Allow-Methods','PUT,POST,GET,DELETE,OPTIONS')
 	res.header('Access-Control-Allow-Headers','x-requested-with, accept, origin, content-type')//处理发送json数据，非简单请求
