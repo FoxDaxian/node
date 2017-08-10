5793409082750eaed6b946c582e3f45e40b4dc0c
a13821190779 <945039036@qq.com>
2017-08-11 00:01:58 +0800

    :sparkles: [views] 增加登录页

--- view/src/views/accountPage/login/index.vue
+++ view/src/views/accountPage/login/index.vue
@@ -4,7 +4,17 @@
 
 <template>
     <div class="wrap">
-        登录
+        <form>
+            <div class="account">
+                <label for="account">账号</label>
+                <input id="account" type="text" v-model="account" @input="replaceCn">
+            </div>
+            <div class="password">
+                <label for="password">密码</label>
+                <input id="password" type="password" v-model="password">
+            </div>
+            <input type="submit" @click.prevent="signin">
+        </form>
     </div>
 </template>
 
@@ -15,8 +25,49 @@
 
         data () {
             return {
-
+                account: '',
+                password: ''
             }
+        },
+        methods: {
+            async signin () {
+                try {
+                    this.progress.start()
+                    const res = await this.$http({
+                        method: 'post',
+                        url: `${this.url}signin`,
+                        body: {
+                            account: this.account,
+                            password: this.password,
+                        }
+                    })
+                    const resDate = res.body.res
+                    switch (res.body.tatus) {
+                        case 0:
+                            console.log('查询时出错')
+                            this.progress.done('fail')
+                        break;
+                        case 1:
+                            this.progress.done()
+                            this.storeCommit('serUserInfo', {...resDate})
+                            this.$router.push({
+                                name: 'home'
+                            })
+                        break;
+                        case 2:
+                            console.log('没有该账户')
+                            this.progress.done('fail')
+                        break;
+                    }
+                } catch (err) {
+                    console.log(err)
+                }
+            },
+            replaceCn () {
+                // 不允许输入中文
+                const reg = /[\u4E00-\u9FA5]{1,}/
+                this.account = this.account.replace(reg, '')
+            },
         }
     }
 </script>
