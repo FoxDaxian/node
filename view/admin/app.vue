438a3af9385196b03eb2f6b8eeb186a4958c24c6
a13821190779 <945039036@qq.com>
2017-08-28 23:49:19 +0800

    :boom: [view] 多入口第一步

---
+++ view/admin/app.vue
@@ -0,0 +1,24 @@
+<style lang="scss" scoped>
+	.adminWrap{
+		color:red;
+	}
+</style>
+
+<template>
+	<div class="adminWrap">
+		这是admin页面
+	</div>
+</template>
+
+<script>
+	export default {
+
+		name: 'admin',
+
+		data () {
+			return {
+
+			};
+		}
+	};
+</script>
