bff8120158628ff2892b1fabc39a6b07f06bb6b7
a13821190779 <945039036@qq.com>
2017-08-30 23:09:07 +0800

    :construction_worker: [cli] 添加admin快捷引用别名

--- view/build/webpack.base.conf.js
+++ view/build/webpack.base.conf.js
@@ -24,6 +24,7 @@ module.exports = {
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
+      '@@': resolve('admin'),
       'common': resolve('src/common/style.scss')
     }
   },
